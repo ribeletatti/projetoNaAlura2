d360406c9ff0cbc0c43643c62d22c5d213071d2a
Richard <richard.silva@grancoffee.com.br>
2023-02-20 12:33:30 -0300

    Terceiro commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:estudos/screen/initial_screen.dart';
+import 'package:estudos/screen/form_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }
